cd5ef9ad4b3429e253465d669d2d9501c9a940aa
none <none>
2023-03-02 11:48:51 +0800

    路由

---
+++ src/pages/error/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ErrorPage: React.FC<any> = () => {
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>路由不存在</p>
+    </div>
+  );
+};
---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import { ErrorPage } from "../pages/error";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: null
+  },
+]);
+
+export default router;
