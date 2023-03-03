d86e1fe838d235496e4106d24bf272dc8016ab6f
none <none>
2023-03-03 09:47:19 +0800

    部门api

---
+++ src/api/department.ts
@@ -0,0 +1,38 @@
+import client from "./internal/httpClient";
+
+export function departmentList() {
+  return client.get("/backend/v1/department/index", {});
+}
+
+export function createDepartment() {
+  return client.get("/backend/v1/department/create", {});
+}
+
+export function storeDepartment(name: string, parentId: number, sort: number) {
+  return client.post("/backend/v1/department/create", {
+    name,
+    parent_id: parentId,
+    sort,
+  });
+}
+
+export function department(id: number) {
+  return client.get(`/backend/v1/department/${id}`, {});
+}
+
+export function updateDepartment(
+  id: number,
+  name: string,
+  parentId: number,
+  sort: number
+) {
+  return client.post(`/backend/v1/department/${id}`, {
+    name,
+    parent_id: parentId,
+    sort,
+  });
+}
+
+export function destroyDepartment(id: number) {
+  return client.destroy(`/backend/v1/department/${id}`);
+}
--- src/api/index.ts
+++ src/api/index.ts
@@ -4,3 +4,6 @@ export * as adminRole from "./admin-role";
 export * as adminUser from "./admin-user";
 export * as courseCategory from "./course-category";
 export * as courseChapter from "./course-chapter";
+export * as course from "./course";
+export * as courseHour from "./course-hour";
+export * as department from "./department";
