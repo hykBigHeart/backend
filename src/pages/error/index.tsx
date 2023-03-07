70a56dbd39c7d99ff0f1269debf6e4c3cd5aaab4
none <none>
2023-03-07 15:09:34 +0800

    优化404页面

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,11 +1,27 @@
+import { Button, Result } from "antd";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./index.module.less";
 
 export const ErrorPage: React.FC<any> = () => {
+  const navigate = useNavigate();
+
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>路由不存在</p>
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="您访问的页面不存在"
+      className={styles["main"]}
+      extra={
+        <Button
+          type="primary"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          返回首页
+        </Button>
+      }
+    />
   );
 };
