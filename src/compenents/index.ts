4231366fae476481ce80b5d9b33d5e3921c8300a
none <none>
2023-03-07 14:59:30 +0800

    组件名重命名

--- src/compenents/index.ts
+++ src/compenents/index.ts
@@ -1,6 +1,6 @@
 export * from "./footer";
 export * from "./header";
-export * from "./leftMenu";
+export * from "./left-menu";
 export * from "./upload-image-button";
 export * from "./treeDepartment";
 export * from "./back-bar";
