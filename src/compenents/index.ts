737340cc5444d5e57f48e4d59240b7c542c324e3
禺狨 <18119604035@163.com>
2023-03-08 10:16:16 +0800

    部门管理优化

--- src/compenents/index.ts
+++ src/compenents/index.ts
@@ -2,5 +2,5 @@ export * from "./footer";
 export * from "./header";
 export * from "./left-menu";
 export * from "./upload-image-button";
-export * from "./treeDepartment";
+export * from "./tree-department";
 export * from "./back-bar";
