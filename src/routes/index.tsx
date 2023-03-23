04872e8becc19f74b1857cecef19831e37148da8
none <none>
2023-03-23 17:32:05 +0800

    fixed: 异步加载闪屏

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,27 +5,44 @@ import { login } from "../api";
 import InitPage from "../pages/init";
 import { getToken } from "../utils";
 
+import LoginPage from "../pages/login";
+import HomePage from "../pages/home";
+import DashboardPage from "../pages/dashboard";
+import ChangePasswordPage from "../pages/change-password";
+import ResourceCategoryPage from "../pages/resource/resource-category";
+import ResourceImagesPage from "../pages/resource/images";
+import ResourceVideosPage from "../pages/resource/videos";
+import CoursePage from "../pages/course";
+import MemberPage from "../pages/member";
+import MemberImportPage from "../pages/member/import";
+import SystemConfigPage from "../pages/system/config";
+import SystemAdministratorPage from "../pages/system/administrator";
+import SystemAdminrolesPage from "../pages/system/adminroles";
+import DepartmentPage from "../pages/department";
+import TestPage from "../pages/test";
+import ErrorPage from "../pages/error";
+
 // 异步加载页面
-const LoginPage = lazy(() => import("../pages/login"));
-const HomePage = lazy(() => import("../pages/home"));
-const DashboardPage = lazy(() => import("../pages/dashboard"));
-const ErrorPage = lazy(() => import("../pages/error"));
-const CoursePage = lazy(() => import("../pages/course"));
-const TestPage = lazy(() => import("../pages/test"));
-const MemberPage = lazy(() => import("../pages/member"));
-const MemberImportPage = lazy(() => import("../pages/member/import"));
-const SystemAdministratorPage = lazy(
-  () => import("../pages/system/administrator")
-);
-const SystemAdminrolesPage = lazy(() => import("../pages/system/adminroles"));
-const DepartmentPage = lazy(() => import("../pages/department"));
-const ChangePasswordPage = lazy(() => import("../pages/change-password"));
-const ResourceImagesPage = lazy(() => import("../pages/resource/images"));
-const ResourceCategoryPage = lazy(
-  () => import("../pages/resource/resource-category")
-);
-const ResourceVideosPage = lazy(() => import("../pages/resource/videos"));
-const SystemConfigPage = lazy(() => import("../pages/system/config"));
+// const LoginPage = lazy(() => import("../pages/login"));
+// const HomePage = lazy(() => import("../pages/home"));
+// const DashboardPage = lazy(() => import("../pages/dashboard"));
+// const ErrorPage = lazy(() => import("../pages/error"));
+// const CoursePage = lazy(() => import("../pages/course"));
+// const TestPage = lazy(() => import("../pages/test"));
+// const MemberPage = lazy(() => import("../pages/member"));
+// const MemberImportPage = lazy(() => import("../pages/member/import"));
+// const SystemAdministratorPage = lazy(
+//   () => import("../pages/system/administrator")
+// );
+// const SystemAdminrolesPage = lazy(() => import("../pages/system/adminroles"));
+// const DepartmentPage = lazy(() => import("../pages/department"));
+// const ChangePasswordPage = lazy(() => import("../pages/change-password"));
+// const ResourceImagesPage = lazy(() => import("../pages/resource/images"));
+// const ResourceCategoryPage = lazy(
+//   () => import("../pages/resource/resource-category")
+// );
+// const ResourceVideosPage = lazy(() => import("../pages/resource/videos"));
+// const SystemConfigPage = lazy(() => import("../pages/system/config"));
 
 let RootPage: any = null;
 if (getToken()) {
