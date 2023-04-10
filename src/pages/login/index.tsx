7bc01276b58e1a6d5231c00de501490f36cac4cc
禺狨 <18119604035@163.com>
2023-04-10 09:11:41 +0800

    后台登录验证码字数验证

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -43,6 +43,10 @@ const LoginPage = () => {
       message.error("请输入图形验证码");
       return;
     }
+    if (captchaVal.length < 4) {
+      message.error("图形验证码错误");
+      return;
+    }
     if (loading) {
       return;
     }
