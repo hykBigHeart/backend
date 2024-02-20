6ccf595984cb37416343d3ff1708da0df3c83db6
xxx <x@x.x>
2024-02-20 15:01:33 +0800

    移除 web-vitals

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import "./assets/iconfont/iconfont.css";
 import "./index.less";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -25,8 +24,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </ConfigProvider>
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
